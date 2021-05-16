0881a2c4b156d147a3ed21bfe4075a4b08297fc3
Carson Burke <carsonburke22@gmail.com>
2021-05-16 15:24:41 -0700

    added sideBar and JS to navBar

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-window.onscroll = function navParentTop() {
+let navTopBar = document.getElementsByClassName("navTopBar")[0]
+let navSideBar = document.getElementsByClassName("navSideBar")[0]
+let navSideBarButton = document.getElementsByClassName("navSideBarButton")[0]
 
-    let navParent = document.getElementsByClassName("navParent")[0]
+let bar1 = document.getElementsByClassName("bar1")[0]
+let bar2 = document.getElementsByClassName("bar2")[0]
+let bar3 = document.getElementsByClassName("bar3")[0]
+
+window.onscroll = function navParentTop() {
 
     let itemPosTop = document.documentElement.scrollTop
 
     if (itemPosTop == 0) {
 
-        navParent.classList.remove("navParentNotTop")
+        navTopBar.classList.remove("navTopBarNotTop")
     } else {
 
-        navParent.classList.add("navParentNotTop")
+        navTopBar.classList.add("navTopBarNotTop")
+    }
+}
+
+function navSideBarToggle() {
+
+    navTopBar.classList.toggle("navTopBarNotTop")
+
+    navSideBar.classList.toggle("navSideBarActive")
+
+    bar1.classList.toggle("barActive1")
+
+    bar2.classList.toggle("barActive2")
+
+    bar3.classList.toggle("barActive3")
+}
+
+window.onclick = element => {
+
+    if (element.target != navSideBar && element.target != navSideBarButton && element.target != bar1 && element.target != bar2 && element.target != bar3) {
+
+        navSideBar.classList.remove("navSideBarActive")
+
+        bar1.classList.remove("barActive1")
+
+        bar2.classList.remove("barActive2")
+
+        bar3.classList.remove("barActive3")
     }
 }
